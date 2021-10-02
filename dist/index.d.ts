3d7863d9525e32fda5734b8e2652b0488acb625c
nakyo <nakyo@whois.me>
2021-10-02 17:22:39 +0900

    remove dist

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const UNNEEDED: boolean;
-export declare type AbstractConstructorType = Function & {
-    prototype: any;
-};
-export interface ConcreteConstructorType {
-    new (...args: any[]): {};
-}
-export declare type ConstructorType = AbstractConstructorType | ConcreteConstructorType;
-export declare const BuiltinClass: () => <T extends ConstructorType>(constructor: T) => T;
