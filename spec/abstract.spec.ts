a0a0308460ed0862df3c065c11dace8145fdaa12
nakyo <nakyo@whois.me>
2021-10-02 15:14:07 +0900

    tslint to eslint

--- spec/abstract.spec.ts
+++ spec/abstract.spec.ts
@@ -1,10 +1,10 @@
-// tslint:disable:max-classes-per-file
 import { BuiltinClass } from '../src';
 
-@BuiltinClass() abstract class Abstract extends Error {}
+@BuiltinClass()
+abstract class Abstract extends Error {}
 class Concreate extends Abstract {}
 
-it("nested", () => {
+it('nested', () => {
   expect(new Concreate() instanceof Concreate).toBe(true);
   expect(new Concreate() instanceof Abstract).toBe(true);
   expect(new Concreate() instanceof Error).toBe(true);
--- spec/nested.spec.ts
+++ spec/nested.spec.ts
@@ -5,7 +5,7 @@ import { BuiltinClass } from '../src';
 class E1 extends Error {}
 class E2 extends E1 {}
 
-it("nested", () => {
+it('nested', () => {
   expect(new E2() instanceof E2).toBe(true);
   expect(new E2() instanceof E1).toBe(true);
   expect(new E2() instanceof Error).toBe(true);
